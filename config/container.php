1ec9ec26f3c930423f426e174baa015b04a84608
Marco Perone <m.perone@mvlabs.it>
2017-05-05 10:40:30 +0200

    create app with fastRoute and Whoops

--- config/container.php
+++ config/container.php
@@ -4,7 +4,20 @@ declare(strict_types=1);
 
 namespace MVLabs\EsCqrsWorkshop;
 
+use Zend\Expressive\Container\ErrorHandlerFactory;
+use Zend\Expressive\Container\WhoopsErrorResponseGeneratorFactory;
+use Zend\Expressive\Container\WhoopsFactory;
+use Zend\Expressive\Container\WhoopsPageHandlerFactory;
+use Zend\Expressive\Middleware\ErrorResponseGenerator;
 use Zend\ServiceManager\ServiceManager;
+use Zend\Stratigility\Middleware\ErrorHandler;
 
-return new ServiceManager();
+return new ServiceManager([
+    'factories' => [
+        ErrorHandler::class => ErrorHandlerFactory::class,
+        ErrorResponseGenerator::class => WhoopsErrorResponseGeneratorFactory::class,
+        'Zend\Expressive\Whoops' => WhoopsFactory::class,
+        'Zend\Expressive\WhoopsPageHandler' => WhoopsPageHandlerFactory::class,
+    ]
+]);
 
--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace MVLabs\EsCqrsWorkshop;
 
+use Zend\Expressive\Application;
+use Zend\Expressive\Router\FastRouteRouter;
+use Zend\Stratigility\Middleware\ErrorHandler;
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
@@ -12,4 +16,17 @@ require __DIR__ . '/../vendor/autoload.php';
 // Self-called anonymous function that creates its own scope and keep the global namespace clean
 (function () {
     $container = require __DIR__ . '/../config/container.php';
+
+    $app = new Application(
+        new FastRouteRouter(),
+        $container
+    );
+
+    // The error handler should be the first (most outer) middleware to catch all Exceptions.
+    $app->pipe(ErrorHandler::class);
+
+    $app->pipeRoutingMiddleware();
+    $app->pipeDispatchMiddleware();
+
+    $app->run();
 })();
