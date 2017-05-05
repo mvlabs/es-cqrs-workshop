051dca2ce1265d278920332f6a8d2408e1a70b23
Marco Perone <m.perone@mvlabs.it>
2017-05-05 17:55:00 +0200

    command bus in container

--- config/container.php
+++ config/container.php
@@ -6,8 +6,16 @@ namespace MVLabs\EsCqrsWorkshop;
 
 use Interop\Container\ContainerInterface;
 use MVLabs\EsCqrsWorkshop\Action\Home;
+use MVLabs\EsCqrsWorkshop\Action\OrderNewPizza;
 use MVLabs\EsCqrsWorkshop\Infrastructure\Renderer\Renderer;
 use MVLabs\EsCqrsWorkshop\Infrastructure\Renderer\HtmlRenderer;
+use Prooph\Common\Event\ActionEvent;
+use Prooph\EventStore\EventStore;
+use Prooph\EventStoreBusBridge\TransactionManager;
+use Prooph\ServiceBus\CommandBus;
+use Prooph\ServiceBus\MessageBus;
+use Prooph\ServiceBus\Plugin\AbstractPlugin;
+use Prooph\ServiceBus\Plugin\ServiceLocatorPlugin;
 use Zend\Expressive\Container\ErrorHandlerFactory;
 use Zend\Expressive\Container\WhoopsErrorResponseGeneratorFactory;
 use Zend\Expressive\Container\WhoopsFactory;
@@ -24,16 +32,50 @@ return new ServiceManager([
         'Zend\Expressive\WhoopsPageHandler' => WhoopsPageHandlerFactory::class,
 
         // ACTIONS
-        Home::class => function (ContainerInterface $container) {
+        Home::class => function (ContainerInterface $container): Home {
             return new Home(
                 $container->get(Renderer::class)
             );
         },
+        OrderNewPizza::class => function (ContainerInterface $container): OrderNewPizza {
+            return new OrderNewPizza();
+        },
 
         // INFRASTRUCTURE
-        Renderer::class => function (ContainerInterface $container) {
+        Renderer::class => function (ContainerInterface $container): Renderer {
             return new HtmlRenderer(__DIR__ . '/../templates/');
+        },
+        CommandBus::class => function (ContainerInterface $container): CommandBus {
+            $commandBus = new CommandBus();
+
+            // this plugin says that the handler for a command is called as the
+            // command itself. This allows to use the command name as a key for the
+            // factory of the command handler in the configuration
+            (new class extends AbstractPlugin {
+                public function attachToMessageBus(MessageBus $messageBus): void
+                {
+                    $this->listenerHandlers[] = $messageBus->attach(
+                        MessageBus::EVENT_DISPATCH,
+                        function (ActionEvent $event) {
+                            $event->setParam(
+                                MessageBus::EVENT_PARAM_MESSAGE_HANDLER,
+                                (string) $event->getParam(MessageBus::EVENT_PARAM_MESSAGE_NAME)
+                            );
+                        }
+                    );
+                }
+            })->attachToMessageBus($commandBus);
+
+            // this plugin is used to retrieve from the dependency injection
+            // container the handler for the command
+            (new ServiceLocatorPlugin($container))->attachToMessageBus($commandBus);
+
+            // this plugin enables transaction handling based on command dispatch
+            (new TransactionManager(
+                $container->get(EventStore::class)
+            ))->attachToMessageBus($commandBus);
+
+            return $commandBus;
         }
     ]
 ]);
-
