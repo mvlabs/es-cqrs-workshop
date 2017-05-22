522e7d4f584d403d583f7f130883b1fcfc9a3f95
Marco Perone <m.perone@mvlabs.it>
2017-05-22 15:27:59 +0200

    complete order projection

--- src/Infrastructure/Projector/RecordPizzeriaOnOrderCompleted.php
+++ src/Infrastructure/Projector/RecordPizzeriaOnOrderCompleted.php
@@ -20,12 +20,22 @@ final class RecordPizzeriaOnOrderCompleted
 
     public function __invoke(OrderCompleted $orderCompleted): void
     {
-        $statement = $this->connection->prepare('');
+        $statement = $this->connection->prepare(
+            'UPDATE pizzerias SET pizzas = (' .
+            '   SELECT to_jsonb(array_agg(ord)) FROM (' .
+            '       SELECT ord FROM	(' .
+            '           SELECT jsonb_array_elements(pizzas) AS ord FROM pizzerias) AS orders '.
+            '           WHERE ord != jsonb_build_object(\'customer\', (:customer)::text, \'at\', (:at)::int, \'pizza\', (:pizza)::text) '.
+            '       ) AS something '.
+            '   )' .
+            'WHERE id = :id'
+        );
 
         $statement->execute([
-            'id' => (string)$orderCompleted->pizzeriaId(),
+            'id' => (string) $orderCompleted->pizzeriaId(),
             'pizza' => $orderCompleted->pizzaTaste(),
-            'customer' => $orderCompleted->customerName()
+            'customer' => $orderCompleted->customerName(),
+            'at' => $orderCompleted->orderCreatedAt()->format('U')
         ]);
     }
 }
